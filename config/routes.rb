917b0d0994d951c4c0c86727303b8865472827d1
Jorge Quinteros <jaquinterosou@gmail.com>
2026-06-04 23:21:53 -0400

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,29 +3,18 @@ Rails.application.routes.draw do
   devise_for :admins
 
   # 2. MUNDO PÚBLICO (La Vitrina - Lo que diseña Pedro)
-  # ¡Aquí configuramos la página de inicio!
   root "products#index" 
-  
-  # Rutas para ver el catálogo y el detalle de una conserva
   resources :products, only: [:index, :show]
 
   # 3. MUNDO PRIVADO (El Panel de UNEL - Donde tú programas)
   namespace :admin do
-    get "orders/index"
-    get "orders/show"
-    get "customers/index"
-    get "customers/show"
-    get "finances/index"
-    get "inventory_movements/index"
-    get "inventory_movements/new"
-    get "ingredients/index"
-    get "ingredients/new"
-    get "ingredients/edit"
-    get "dashboard/index"
-    # Esto genera automáticamente todas las rutas CRUD (Crear, Leer, Actualizar, Borrar)
-    resources :products
+    get 'dashboard', to: 'dashboard#index'
     
-    # Si quieres que el admin tenga un dashboard inicial al entrar a /admin
-    # get 'dashboard', to: 'dashboard#index'
+    resources :products
+    resources :ingredients
+    resources :inventory_movements, only: [:index, :new, :create]
+    resources :finances, only: [:index]
+    resources :customers, only: [:index, :show]
+    resources :orders, only: [:index, :show, :update]
   end
 end
