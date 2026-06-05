d03c04b71bbbea9a1a4d1e831110191d67bb9418
PQuinteroso <pquinterosoweb@gmail.com>
2026-06-04 21:49:14 -0400

    Clase activa de sidebar lista. Cambios pequeños

--- app/javascript/controllers/admin_sidebar_controller.js
+++ app/javascript/controllers/admin_sidebar_controller.js
@@ -1,7 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["sidebar", "label", "compactLabel"]
+  static targets = ["sidebar", "label"]
+
+  connect() {
+    this.highlightActiveLink()
+  }
 
   toggle() {
     this.sidebarTarget.classList.toggle("w-64")
@@ -11,9 +15,50 @@ export default class extends Controller {
     this.labelTargets.forEach((label) => {
       label.classList.toggle("hidden")
     })
+  }
 
-    this.compactLabelTargets.forEach((label) => {
-      label.classList.toggle("hidden")
+  highlightActiveLink() {
+    const currentPath = window.location.pathname
+    const links = this.element.querySelectorAll("[data-active-path]")
+
+    links.forEach((link) => {
+      const activePath = link.dataset.activePath
+
+      if (currentPath.startsWith(activePath)) {
+        this.activateLink(link)
+      } else {
+        this.deactivateLink(link)
+      }
     })
+  }
+
+  activateLink(link) {
+    link.classList.remove(
+      "bg-unel-verde",
+      "text-unel-blanco",
+      "text-unel-crema",
+      "border-transparent"
+    )
+
+  link.classList.add(
+      "text-unel-dorado-2",
+      "border-unel-dorado-2"
+    )
+  }
+
+  deactivateLink(link) {
+    link.classList.remove(
+      "bg-unel-verde",
+      "text-unel-blanco",
+      "text-unel-dorado",
+      "border-unel-dorado"
+    )
+
+    link.classList.add(
+      "text-unel-crema",
+      "border-transparent",
+      "hover:border-unel-dorado",
+      "hover:text-unel-dorado"
+    )
   }
 }
