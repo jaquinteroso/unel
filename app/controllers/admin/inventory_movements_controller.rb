99f29595dc066a59537353e24594797dd41fc939
PQuinteroso <pquinterosoweb@gmail.com>
2026-06-27 19:05:13 -0400

    Layout inicial de inventario

--- app/controllers/admin/inventory_movements_controller.rb
+++ app/controllers/admin/inventory_movements_controller.rb
@@ -1,5 +1,14 @@
 class Admin::InventoryMovementsController < Admin::ApplicationController
   def index
+    @products = Product
+      .includes(
+        { image_attachment: :blob },
+        { recipe_items: :ingredient },
+        { product_supplies: :supply }
+      )
+      .order(:name)
+
+    @low_stock_threshold = 5
   end
 
   def new
