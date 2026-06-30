f74b4e9834d9c91474a211c4bcad6e4fcde66fd3
PQuinteroso <pquinterosoweb@gmail.com>
2026-06-30 03:50:16 -0400

    Movimientos y deudas agregados a finanzas

---
+++ app/controllers/admin/debts_controller.rb
@@ -0,0 +1,7 @@
+class Admin::DebtsController < Admin::ApplicationController
+  def index
+  end
+
+  def new
+  end
+end
---
+++ app/controllers/admin/financial_movements_controller.rb
@@ -0,0 +1,12 @@
+class Admin::FinancialMovementsController < Admin::ApplicationController
+  def new
+    @movement_type = normalized_movement_type(params[:movement_type])
+  end
+
+  private
+
+  def normalized_movement_type(value)
+    allowed_types = [ "income", "expense" ]
+    allowed_types.include?(value) ? value : "income"
+  end
+end
